c028d06da0d314e1f60747ae6161391e47e964e6
Clara <144264170+clara-tyman@users.noreply.github.com>
2024-05-07 04:28:48 +0000

    all code

--- js/script.js
+++ js/script.js
@@ -3,3 +3,17 @@
 // Created by: Mr. Coxall
 // Created on: Sep 2020
 // This file contains the JS functions for index.html
+
+function myButtonClicked() {
+  // input
+  const tuesday = document.getElementById("tuesday").checked
+  const thursday = document.getElementById("thursday").checked
+  const age = parseInt(document.getElementById("user-age").value)
+
+  // process
+  if ((tuesday == true) || (thursday == true) || ((age > 12) && (age < 21))) {
+    document.getElementById("answer").innerHTML = "You are eligible student pricing!"
+  } else {
+    document.getElementById("answer").innerHTML = "You will have to pay the normal fee."
+  }
+}
